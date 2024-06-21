7c83c995ee004b8bb6648d743e5b77abc5aec277
Georgey764 <georgesamuel764@gmail.com>
2024-06-21 10:27:54 -0500

    Update query_chapter.py
    Updated the output of the query chapter to be well-formatted.

--- query_chapter.py
+++ query_chapter.py
@@ -1,4 +1,4 @@
-import json, hashlib, math, os, re
+import json, hashlib, math, os, re, sys
 from docx import Document
 
 _stop_words = {"a","about","above","after","again","against","all","am","an","and","any","are","aren't","as","at","be","because","been","before","being","below","between","both","but","by","can't","cannot","could","couldn't","did","didn't","do","does","doesn't","doing","don't","down","during","each","few","for","from","further","had","hadn't","has","hasn't","have","haven't","having","he","he'd","he'll","he's","her","here","here's","hers","herself","him","himself","his","how","how's","i","i'd","i'll","i'm","i've","if","in","into","is","isn't","it","it's","its","itself","let's","me","more","most","mustn't","my","myself","no","nor","not","of","off","on","once","only","or","other","ought","our","ours","ourselves","out","over","own","same","shan't","she","she'd","she'll","she's","should","shouldn't","so","some","such","than","that","that's","the","their","theirs","them","themselves","then","there","there's","these","they","they'd","they'll","they're","they've","this","those","through","to","too","under","until","up","very","was","wasn't","we","we'd","we'll","we're","we've","were","weren't","what","what's","when","when's","where","where's","which","while","who","who's","whom","why","why's","with","won't","would","wouldn't","you","you'd","you'll","you're","you've","your","yours","yourself","yourselves"}
@@ -15,7 +15,7 @@ def _make_hash_name(term, max_len=4):
 
 def _get_bm25_score(document_length, num_of_doc_containing_key, keyword_freq_in_doc):
 	idf = math.log(((_total_num_files - num_of_doc_containing_key + 0.5) / (num_of_doc_containing_key + 0.5)) + 1)
-	k1 = 0
+	k1 = 0.01
 	b = 0
 	avg_len_of_docs = 800
 	second_part = (keyword_freq_in_doc * (k1 + 1)) /  ((keyword_freq_in_doc + k1) * (1 - b + (b * (document_length / avg_len_of_docs))))
@@ -27,7 +27,6 @@ def _parse_query(query, bm25_scores):
 		keyword = keyword.strip(" \t\n0123456789.:[(<>)].,'\"‘’?--•…")
 		if not keyword.lower() in _stop_words and keyword:
 			keyword = keyword[:min(max_ngram, len(keyword))]
-			print(keyword)
 			file_name = _make_hash_name(keyword.lower())
 			if os.path.exists(f"./hashed_documents/{file_name}"):
 				with open(f"./hashed_documents/{file_name}") as file:
@@ -97,11 +96,17 @@ def return_preview_indices(query, book, chapter):
 	return maxVal
 
 def main():
-	query = """
-	In the beginning the god created the sky
-	"""
-	result = return_bm25_scores(query)
-	print(result)
+	query = sys.argv[1]
+	max_result = 10
+	if len(sys.argv) < 2:
+		max_result = int(sys.argv[2])
+	result = return_bm25_scores(query)[:max_result]
+	print(f"\nThe following is the list of top {max_result} results for your query ranging from the best match to the worst match.\n")
+	print(f"\n{"No.":<5} {"Book":<16} {"Chapter":<8} Search Score")
+	for index, tlist in enumerate(result):
+		no = str(index + 1) + "."
+		print("{:<5} {:<16} {:<8} {}".format(no, tlist[0].split("_")[0], tlist[0].split("_")[2][:1], str(tlist[1])))
+	print("\n")
 	
 
 if __name__ == "__main__":
